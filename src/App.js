b743a98699f5cd115259cd3ae46cc5dda5a4e0a5
Rastacool <romain.grillon95@gmail.com>
2023-01-16 11:35:10 +0100

    🔥 corection app.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import LoginPage from './components/pages/login/LoginPage';
 
 function App() {
 
-  // state (état, donnée)
-  const [prenom, setPrenom] = useState("RASTA")
-  const [count, setCount] = useState(8)
-  // comportements
-
-  const handleClick = () => {
-
-    alert('Please fill in this field.')
-  }
   //affichage
 
   return (
