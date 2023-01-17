6e3b17e38e28c2bd2cc74da393f978532acd14b3
Rastacool <romain.grillon95@gmail.com>
2023-01-17 01:06:53 +0100

    ✨ F02: APP installation du systeme de routing

---
+++ src/components/pages/order/OrderPage.js
@@ -0,0 +1,14 @@
+
+export default function OrderPage() {
+  //state
+  
+  //comportements
+
+  return (
+    <form>
+      <h1>Bonjour bob</h1>
+
+      <button >Déconnexion</button>
+    </form>
+  );
+}
