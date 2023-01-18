0773576e52c6f0132cfd8b25049c6bcf7365bc63
Rastacool <romain.grillon95@gmail.com>
2023-01-18 11:43:38 +0100

    🎨 F03 projet av correction

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,27 +1,38 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../../../design";
+import { FaBeer } from "react-icons/fa";
 
 export default function LoginForm() {
   //state
   const [inputValue, setInputValue] = useState("");
- const  navigate = useNavigate()
+  const navigate = useNavigate();
 
   //comportements
   const handleSubmit = (event) => {
     event.preventDefault();
     setInputValue("");
-    navigate(`order/${inputValue}`)
+    navigate(`order/${inputValue}`);
   };
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
   };
-
   return (
-    <form action="Submit" onSubmit={handleSubmit} >
-      <h1>Bienvenue chez nous !</h1>
+    <LoginFormStyled action="Submit" onSubmit={handleSubmit}>
+      <h1>
+        CRAZEE <img src="./logo.png" alt="burger-logo" />
+        BURGER{" "}
+      </h1>
       <br />
-      <h2>Connectez-vous</h2>
+
+      <h2>
+
+        CONNECTER-VOUS
+      </h2>
+      <div>
+      <FaBeer />
       <input
         value={inputValue}
         onChange={handleChange}
@@ -29,7 +40,28 @@ export default function LoginForm() {
         placeholder="Entrer votre prénom..."
         required
       />
+      </div>
+      <br />
       <button>Accédez à votre espace</button>
-    </form>
+    </LoginFormStyled>
   );
 }
+
+const LoginFormStyled = styled.form`
+  background-image: url(burger-background.jpg);
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  h1 {
+    color: ${theme.colors.primary_burger};
+    text-align: center;
+  }
+  h2 {
+    text-align: center;
+    color: ${theme.colors.white};
+  }
+`;
