fb922d71c4aabff0aeb9d20e81213c3f7283459f
Rastacool <romain.grillon95@gmail.com>
2023-01-18 12:29:16 +0100

    💄 F03: Login> Add styling and background

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,10 +1,35 @@
+import styled from "styled-components";
+import Logo from "../../reusable-ui/Logo";
 import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   //affichage
   return (
-    <div>
+    <LoginPageStyle>
+      <Logo/>
       <LoginForm />
-    </div>
+    </LoginPageStyle>
   );
 }
+const LoginPageStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  ::before{
+    content: "";
+  background: url("/images/burger-background.jpg");
+  background-size: cover;
+  background-position: center;
+  background-blend-mode: darken;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index  : -1;
+  }
+`;
